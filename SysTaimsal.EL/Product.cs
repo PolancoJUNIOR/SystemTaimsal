334c2e9be0f33af823a5c6c6c091e1bee930f993
REMR11 <remr11@outlook.com>
2022-11-25 09:23:19 -0600

    V0.0.0.2 Update EL 3 class

---
+++ SysTaimsal.EL/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysTaimsal.EL
+{
+    public class Product
+    {
+        [Key]
+        public int IdProduct { get; set; }
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(40, ErrorMessage = "Maximo 40 caracteres")]
+        [Display(Name ="Producto")]
+        public string? NameProduct { get; set; }
+        [Required(ErrorMessage ="Imagen necesaria")]
+        public string? ImageProduct { get; set; }
+        [StringLength(500, ErrorMessage ="Maximo 500 caracteres")]
+        public string? DescriptionProduct { get; set; }
+        [Required(ErrorMessage ="Precio es obligatorio")]
+        [Display(Name ="Precio")]
+        public decimal? Price { get; set; }
+    }
+}
--- SysTaimsal.EL/Provider.cs
+++ SysTaimsal.EL/Provider.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,15 @@ namespace SysTaimsal.EL
 {
     public  class Provider
     {
+        [Key]
         public int IdProvider { get; set; }
+        [ForeignKey("Machine")]
+        [Required(ErrorMessage = "Machine es obligatorio")]
+        [Display(Name = "Machine")]
         public int? IdMachine { get; set; }
+        [Required(ErrorMessage ="Nombre es obligatorio")]
+        [StringLength(40, ErrorMessage ="Maximo 40 caracteres")]
+        [Display(Name = "Nombre de producto")]
         public string? NameProvider { get; set; }
     }
 }
