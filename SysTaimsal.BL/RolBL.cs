e18da6e8d65f7a775b315cb7cd713c41171218cb
REMR11 <remr11@outlook.com>
2022-12-03 11:24:00 -0600

    V 0.0.0.7 Init BL with RolBL

---
+++ SysTaimsal.BL/RolBL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SysTaimsal.EL;
+using SysTaimsal.DAL;
+
+namespace SysTaimsal.BL
+{
+    public class RolBL
+    {
+        public async Task<int> CreateAsync(Rol pRol) { 
+            return await RolDAL.CrearteAsync(pRol);
+        }
+
+        public async Task<int> ModifyAsync(Rol pRol) { 
+            return await RolDAL.ModifyAsync(pRol);
+        }
+
+        public async Task<Rol> GetByIdAsync(Rol pRol) {
+            return await RolDAL.GetByIdAsync(pRol);
+        }
+
+        public async Task<List<Rol>> GetAllAsync()
+        {
+            return await RolDAL.GetAllAsync();
+        }
+
+        public async Task<List<Rol>>SearchAsync (Rol pRol) {
+            return await RolDAL.SearchAsync(pRol);
+        }
+
+        public async Task<int> DeleteAsync(Rol pRol) { 
+            return await RolDAL.DeleteAsync(pRol);
+        }
+    }
+}
